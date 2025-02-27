c9ac04a142ade93fa0e7fe4bc7b1ed49a3787959
MaximeFantin <maxime.fantin.36@gmail.com>
2025-02-27 15:28:19 +0100

    Add files via upload

---
+++ IJVM-decompiler.py
@@ -0,0 +1,11 @@
+
+
+def decompile(bytecode: str) -> str:
+    """
+    Decompiles the given IJVM bytecode into a human-readable format.
+
+    :param bytecode: The IJVM bytecode to decompile.
+    :return: The decompiled IJVM bytecode.
+    """
+
+    
